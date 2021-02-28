932760c6a39dfd0dff31a9c3dab6392abf133d39
Yutthana Siphuengchai <yutthana.s@outlook.com>
2021-03-01 00:18:28 +0700

    Add Play font.

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ export default function Home() {
     <>
       <Head>
         <title>Matching Game Card</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" />
       </Head>
       <h1>This website is under construction.</h1>
       <p>Coming soon...</p>

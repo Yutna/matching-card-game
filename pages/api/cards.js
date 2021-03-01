dae8e434ecadbd81e219222b03de2862e07f3832
Yutthana Siphuengchai <yutthana.s@outlook.com>
2021-03-01 20:17:38 +0700

    Implement matching card game.

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -2,18 +2,18 @@ function cards(req, res) {
   const cards = {
     globalBest: 20,
     items: [
-      { id: 1, number: 1 },
-      { id: 2, number: 1 },
-      { id: 3, number: 2 },
-      { id: 4, number: 2 },
-      { id: 5, number: 3 },
-      { id: 6, number: 3 },
-      { id: 7, number: 4 },
-      { id: 8, number: 4 },
-      { id: 9, number: 5 },
-      { id: 10, number: 5 },
-      { id: 11, number: 6 },
-      { id: 12, number: 6 },
+      { id: 1, number: 1, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 2, number: 1, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 3, number: 2, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 4, number: 2, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 5, number: 3, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 6, number: 3, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 7, number: 4, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 8, number: 4, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 9, number: 5, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 10, number: 5, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 11, number: 6, open: false, matched: false, unmatched: false, disabled: false },
+      { id: 12, number: 6, open: false, matched: false, unmatched: false, disabled: false },
     ]
   };
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import _ from 'lodash';
+import classNames from 'classnames/bind';
 import CardsAdapter from '../adapters/CardsAdapter';
 import layoutStyles from '../styles/layout.module.scss';
 import headerStyles from '../styles/header.module.scss';
@@ -9,15 +10,21 @@ import cardStyles from '../styles/card.module.scss';
 import panelStyles from '../styles/panel.module.scss';
 import buttonStyles from '../styles/button.module.scss';
 
+const cardStylesContext = classNames.bind(cardStyles);
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleCardClick = this.handleCardClick.bind(this);
+    this.handleNewGameButton = this.handleNewGameButton.bind(this);
 
     this.state = {
       click: 0,
       myBest: 0,
       globalBest: 0,
-      cards: []
+      cards: [],
+      openedCards: []
     };
   }
 
@@ -26,11 +33,151 @@ class Home extends React.Component {
   componentDidMount() {
     CardsAdapter.get().then((data) => {
       const { globalBest, items } = data;
+      const cards = _.shuffle([...items]);
+
+      this.setState({ globalBest, cards });
+    });
+  }
+
+  // Event handlers
+
+  handleCardClick(targetCard) {
+    const { click, cards, openedCards } = this.state;
+    const newClickScore = click + 1;
+
+    cards.forEach((card) => {
+      card.disabled = true;
+
+      if (card.id === targetCard.id) {
+        card.open = true;
+        openedCards.push(card);
+      }
+    });
+
+    this.setState({
+      click: newClickScore,
+      cards,
+      openedCards
+    }, () => {
+      this.checkMatchedOrUnmatchedOpenCards();
+    });
+  }
+
+  handleNewGameButton() {
+    const { globalBest } = this.state;
+
+    CardsAdapter.get().then((data) => {
+      const { items } = data;
       const cards = _.shuffle(items);
 
-      this.setState({ globalBest, cards }, () => {
-        console.log(cards);
+      this.setState({
+        click: 0,
+        myBest: 0,
+        globalBest,
+        cards,
+        openedCards: []
       });
+    });
+  }
+
+  // Component methods
+
+  checkMatchedOrUnmatchedOpenCards() {
+    const { cards, openedCards } = this.state;
+    const cardIds = openedCards.map((card) => card.id);
+
+    if (openedCards.length !== 2) {
+      cards.forEach((card) => {
+        if (cardIds.includes(card.id) || (card.open && card.matched)) {
+          card.disabled = true;
+        } else {
+          card.disabled = false;
+        }
+      });
+
+      this.setState({ cards });
+      return;
+    }
+
+    const firstCard = openedCards[0];
+    const secondCard = openedCards[1];
+
+    if ((firstCard.number === secondCard.number)) {
+      this.matched();
+    } else {
+      this.unmatched();
+    }
+  }
+
+  matched() {
+    const { cards, openedCards } = this.state;
+    const cardIds = openedCards.map((card) => card.id);
+
+    cards.forEach((card) => {
+      if (cardIds.includes(card.id)) {
+        card.matched = true;
+        card.disabled = true;
+      } else if (card.open && card.matched) {
+        card.disabled = true;
+      } else {
+        card.disabled = false;
+      }
+    });
+
+    this.setState({ cards, openedCards: [] }, () => {
+      this.updatePanel();
+    });
+  }
+
+  unmatched() {
+    const { cards, openedCards } = this.state;
+    const cardIds = openedCards.map((card) => card.id);
+
+    cards.forEach((card) => {
+      if (cardIds.includes(card.id)) {
+        card.unmatched = true;
+      }
+    });
+
+    this.setState({ cards }, () => {
+      this.closeCards();
+    });
+  }
+
+  closeCards() {
+    const { cards, openedCards } = this.state;
+    const cardIds = openedCards.map((card) => card.id);
+
+    cards.forEach((card) => {
+      if (cardIds.includes(card.id)) {
+        card.open = false;
+        card.unmatched = false;
+        card.disabled = false;
+      } else if (card.open && card.matched) {
+        card.disabled = true;
+      } else {
+        card.disabled = false;
+      }
+    });
+
+    setTimeout(() => {
+      this.setState({ cards, openedCards: [] });
+    }, 375);
+  }
+
+  updatePanel() {
+    const { click, cards } = this.state;
+    const clickScore = click;
+    const matchedCardIds = cards.filter((card) => card.matched).map((card) => card.id);
+
+    if (matchedCardIds.length !== cards.length) {
+      return;
+    }
+
+    this.setState({
+      click: 0,
+      myBest: clickScore,
+      globalBest: clickScore
     });
   }
 
@@ -38,6 +185,20 @@ class Home extends React.Component {
 
   displayScore(score) {
     return score === 0 ? '-' : score;
+  }
+
+  displayCardNumber(card) {
+    return card.open ? card.number : '';
+  }
+
+  getCardStyles(card) {
+    return cardStylesContext({
+      card: true,
+      cardOpen: card.open,
+      cardMatched: card.matched,
+      cardUnmatched: card.unmatched,
+      cardDisabled: card.disabled
+    });
   }
 
   render() {
@@ -82,15 +243,15 @@ class Home extends React.Component {
               </em>
             </div>
             <div className={panelStyles.panel}>
-              <button type="button" className={buttonStyles.button}>
+              <button type="button" className={buttonStyles.button} onClick={this.handleNewGameButton}>
                 Start New Game
               </button>
             </div>
           </div>
           <div className={deskStyles.deskBoard}>
             {cards.map((card) => (
-              <div key={card.id} className={cardStyles.card}>
-                {card.number}
+              <div key={card.id} className={this.getCardStyles(card)} onClick={() => this.handleCardClick(card)}>
+                {this.displayCardNumber(card)}
               </div>
             ))}
           </div>
